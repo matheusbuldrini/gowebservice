28ae343e293d3eb9ae99c3786750e4980eddeb57
Matheus Buldrini <38816666+matheusbuldrini@users.noreply.github.com>
2023-04-14 22:58:42 -0300

    Adding get, post, put delete

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	Id        int
 	FirstName string
@@ -16,8 +21,43 @@ func GetUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.Id != 0 {
+		return User{}, errors.New("New user must not include id")
+	}
 	u.Id = nextId
 	nextId++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserById(id int) (User, error) {
+	for _, u := range users {
+		if u.Id == id {
+			return *u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with Id '%v' not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.Id == u.Id {
+			users[i] = &u
+			return u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with Id '%v' not found", u.Id)
+}
+
+func RemoveUserById(id int) error {
+	for i, u := range users {
+		if u.Id == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("User with Id '%v' not found", id)
 }
